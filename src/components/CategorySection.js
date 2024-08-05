32c52f1bc5b51c97b8bd0c0939610be7ea6d0edc
vinoth686 <57247148+vinoth686@users.noreply.github.com>
2024-08-05 09:20:42 +0100

    Update CategorySection.js

--- src/components/CategorySection.js
+++ src/components/CategorySection.js
@@ -1,32 +1,3 @@
-// import React from 'react';
-// import BeachAccessIcon from '@mui/icons-material/BeachAccess';
-// import CabinIcon from '@mui/icons-material/Cabin';
-// import PoolIcon from '@mui/icons-material/Pool';
-// import HomeIcon from '@mui/icons-material/Home';
-// import ApartmentIcon from '@mui/icons-material/Apartment';
-
-// const categories = [
-//   { name: 'Beach', icon: <BeachAccessIcon /> },
-//   { name: 'Cabins', icon: <CabinIcon /> },
-//   { name: 'Pools', icon: <PoolIcon /> },
-//   { name: 'Homes', icon: <HomeIcon /> },
-//   { name: 'Apartments', icon: <ApartmentIcon /> },
-// ];
-
-// const CategorySection = () => {
-//   return (
-//     <div className="flex justify-center space-x-6 mt-6">
-//       {categories.map((category) => (
-//         <div key={category.name} className="flex flex-col items-center">
-//           <div className="bg-gray-200 p-3 rounded-full">{category.icon}</div>
-//           <span className="mt-2">{category.name}</span>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default CategorySection;
 import React from 'react';
 import BeachAccessIcon from '@mui/icons-material/BeachAccess';
 import CabinIcon from '@mui/icons-material/Cabin';
