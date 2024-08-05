e492c0ea17d2eb15aad1d36ee5648ef545022e79
vinoth686 <57247148+vinoth686@users.noreply.github.com>
2024-08-05 09:22:36 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,6 @@
-// import React from 'react';
-// import Button from '@mui/material/Button';
-
-// function App() {
-//   return (
-//     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-//       <h1 className="text-3xl font-bold text-blue-600">Hello, Tailwind CSS and MUI!</h1>
-//       <Button variant="contained" color="primary" className="mt-4">
-//         MUI Button
-//       </Button>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
 import React from 'react';
 import HomePage from './components/HomePage';
-import './index.css';  // Ensure Tailwind CSS styles are applied
+import './index.css';
 
 function App() {
   return (
